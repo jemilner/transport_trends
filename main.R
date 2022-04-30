10ac391432541fa03156bc4674f97e507be6f608
jemilner <37807444+jemilner@users.noreply.github.com>
2022-04-30 17:45:46 +0100

    join data

--- main.R
+++ main.R
@@ -7,3 +7,16 @@ population_file <- "data/population_clean.csv"
 
 transport_tbl <- read_csv(file = transport_file)
 population_tbl <- read_csv(file = population_file)
+
+summary(transport_tbl)
+summary(population_tbl)
+
+#join region name and pop count from population_tbl to transport_tbl
+combo_tbl <- transport_tbl %>%
+    left_join(
+        population_tbl,
+        by = c("ons_code" = "Code", "year" = "Year")
+    ) %>%
+    arrange(year, region_id)
+
+
