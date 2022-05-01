b9db00af0698353f4779c177844aa03f03e8fdcf
jemilner <37807444+jemilner@users.noreply.github.com>
2022-05-01 09:40:26 +0100

    national summary table

--- R/helper_fns.R
+++ R/helper_fns.R
@@ -3,9 +3,19 @@
 #' @param data_tbl 
 #' @param region A region id (integer)
 #' @param col A column name in data_tbl 
-get_baseline <- function(data_tbl, region, col) {
+get_baseline_region <- function(data_tbl, region, col) {
     data_tbl %>%
         filter(region_id == region) %>%
+        filter(year == min(year)) %>%
+        pull(col)
+}
+
+#' Get earliest value of col
+#'
+#' @param data_tbl 
+#' @param col A column name in data_tbl 
+get_baseline_national <- function(data_tbl, col) {
+    data_tbl %>%
         filter(year == min(year)) %>%
         pull(col)
 }
--- main.R
+++ main.R
@@ -13,11 +13,11 @@ population_tbl <- read_csv(file = population_file) %>%
     rename("population" = "All Persons")
 
 
-summary(transport_tbl)
-summary(population_tbl)
+# summary(transport_tbl)
+# summary(population_tbl)
 
 #join region name and pop count from population_tbl to transport_tbl
-combo_tbl <- transport_tbl %>%
+regional_tbl <- transport_tbl %>%
     left_join(
         population_tbl,
         by = c("ons_code" = "Code", "year" = "Year")
@@ -25,11 +25,40 @@ combo_tbl <- transport_tbl %>%
     arrange(year, region_id)
 
 #create baseline comparison
-combo_tbl <- combo_tbl %>%
-    mutate(pedal_cycles_change = round(pedal_cycles / get_baseline(combo_tbl, region_id, "pedal_cycles"), 2)) %>%
-    mutate(cars_and_taxis_change = round(cars_and_taxis / get_baseline(combo_tbl, region_id, "cars_and_taxis"), 2)) %>%
-    mutate(buses_and_coaches_change = round(buses_and_coaches / get_baseline(combo_tbl, region_id, "buses_and_coaches"), 2)) %>%
-    mutate(lgvs_change = round(lgvs / get_baseline(combo_tbl, region_id, "lgvs"), 2)) %>%
-    mutate(all_hgvs_change = round(all_hgvs / get_baseline(combo_tbl, region_id, "all_hgvs"), 2)) %>%
-    mutate(all_motor_vehicles_change = round(all_motor_vehicles / get_baseline(combo_tbl, region_id, "all_motor_vehicles"), 2)) %>%
-    mutate(population_change = round(population / get_baseline(combo_tbl, region_id, "population"), 2))
+regional_tbl <- regional_tbl %>%
+    mutate(pedal_cycles_change = round(pedal_cycles / get_baseline_region(regional_tbl, region_id, "pedal_cycles"), 2)) %>%
+    mutate(cars_and_taxis_change = round(cars_and_taxis / get_baseline_region(regional_tbl, region_id, "cars_and_taxis"), 2)) %>%
+    mutate(buses_and_coaches_change = round(buses_and_coaches / get_baseline_region(regional_tbl, region_id, "buses_and_coaches"), 2)) %>%
+    mutate(lgvs_change = round(lgvs / get_baseline_region(regional_tbl, region_id, "lgvs"), 2)) %>%
+    mutate(all_hgvs_change = round(all_hgvs / get_baseline_region(regional_tbl, region_id, "all_hgvs"), 2)) %>%
+    mutate(all_motor_vehicles_change = round(all_motor_vehicles / get_baseline_region(regional_tbl, region_id, "all_motor_vehicles"), 2)) %>%
+    mutate(population_change = round(population / get_baseline_region(regional_tbl, region_id, "population"), 2))
+
+#filter on just England
+regional_tbl <- regional_tbl %>%
+    slice(grep("^E", ons_code))
+
+#exploratory plot
+# ggplot(regional_tbl, aes(x = year, y = population_change, group = region_name)) +
+#     geom_line(aes(color = as.factor(region_name)))
+
+#national tbl
+national_tbl <- regional_tbl %>%
+    group_by(year) %>%
+    summarise(
+        population = sum(population),
+        pedal_cycles = sum(pedal_cycles),
+        cars_and_taxis = sum(cars_and_taxis),
+        buses_and_coaches = sum(buses_and_coaches),
+        lgvs = sum(lgvs),
+        all_hgvs = sum(all_hgvs),
+        all_motor_vehicles = sum(all_motor_vehicles)
+    ) %>%
+    mutate(population_change = round(population / get_baseline_national(national_tbl, "population"), 2)) %>%
+    mutate(pedal_cycles_change = round(pedal_cycles / get_baseline_national(national_tbl, "pedal_cycles"), 2)) %>%
+    mutate(cars_and_taxis_change = round(cars_and_taxis / get_baseline_national(national_tbl, "cars_and_taxis"), 2)) %>%
+    mutate(buses_and_coaches_change = round(buses_and_coaches / get_baseline_national(national_tbl, "buses_and_coaches"), 2)) %>%
+    mutate(lgvs_change = round(lgvs / get_baseline_national(national_tbl, "lgvs"), 2)) %>%
+    mutate(all_hgvs_change = round(all_hgvs / get_baseline_national(national_tbl, "all_hgvs"), 2)) %>%
+    mutate(all_motor_vehicles_change = round(all_motor_vehicles / get_baseline_national(national_tbl, "all_motor_vehicles"), 2))
+
